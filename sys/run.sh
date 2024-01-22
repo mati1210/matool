9ff72145e90082cce88fcfd24089dc508214d5e1
mat1210 <matheuz1210@gmail.com>
2024-01-21 23:19:47 -0300

    copy dotfiles into sys

--- sys/run.sh
+++ sys/run.sh
@@ -2,4 +2,11 @@
 cd ${0:h}
 DIR="/tmp/matool.mkosi.$USER"
 
+cp dotfiles/.config/tmux/tmux.conf mkosi.extra/etc/tmux.conf
+for f ( zsh{env,rc} ) {
+	cp dotfiles/.config/zsh/.$f mkosi.extra/etc/zsh/$f
+}
+cp -a dotfiles/.config/zsh/{utils,zsh-syntax-highlighting}  mkosi.extra/etc/zsh/
+echo 'ZDOTDIR=/etc/zsh' >> mkosi.extra/etc/zsh/zshenv
+
 exec mkosi --cache-dir "$DIR/cache" --workspace-dir "$DIR/workspace" "$@"
